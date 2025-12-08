7a28bbdf8d0c1a036c1715a7548b7dd1283ba8b2
Alisson <alisontattooskate@gmail.com>
2025-12-07 22:43:31 -0300

    feat: delete docs

--- src/sql/00_create_schemas_and_tables.sql
+++ src/sql/00_create_schemas_and_tables.sql
@@ -1,5 +1,4 @@
 -- Script de criação de schemas e tabelas
--- Este script é executado automaticamente pelo pipeline ETL < -----
 
 -- Schemas já foram criados pelo python mas vamos garantimos aqui também
 CREATE SCHEMA IF NOT EXISTS raw;
